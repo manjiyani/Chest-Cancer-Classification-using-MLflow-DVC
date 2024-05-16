f7c0c8efec15845ad95ca383b826918788c49751
manjiyani <manjiyani.alim@gmail.com>
2024-05-16 11:59:50 -0400

    Added DVC support and Mlflow-fixes

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 # STAGE_NAME = "Data Ingestion stage"
 
@@ -14,16 +16,16 @@ from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 #     logger.exception(e)
 #     raise e
 
-# STAGE_NAME = "Prepare base model"
-# try: 
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    prepare_base_model = PrepareBaseModelTrainingPipeline()
-#    prepare_base_model.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 STAGE_NAME = "Training"
 try: 
@@ -32,6 +34,22 @@ try:
    model_trainer = ModelTrainingPipeline()
    model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/manjiyani/Chest-Cancer-Classification-using-MLflow-DVC.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"]="manjiyani"
+os.environ["MLFLOW_TRACKING_PASSWORD"]="64664aee3c04d16d8c20ea60291bc61b86016836"
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
 except Exception as e:
         logger.exception(e)
         raise e
--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -23,8 +23,6 @@ class PrepareBaseModelConfig:
     params_classes: int
 
 
-
-
 @dataclass(frozen=True)
 class TrainingConfig:
     root_dir: Path
@@ -35,8 +33,6 @@ class TrainingConfig:
     params_batch_size: int
     params_is_augmentation: bool
     params_image_size: list
-
-
 
 @dataclass(frozen=True)
 class EvaluationConfig:
